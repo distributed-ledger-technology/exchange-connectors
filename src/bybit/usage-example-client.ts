e5e8fa5ac7695abeb2b367e28cf0fead1f513f49
Michael <michael.spengler@sap.com>
2021-09-28 18:42:04 +0200

    add example client

---
+++ src/bybit/usage-example-client.ts
@@ -0,0 +1,38 @@
+import { IExchangeConnector } from "../interfaces/exchange-connector-interface.ts"
+import { BybitConnector } from "./bybit-connector.ts"
+
+
+export class ExampleClient {
+
+
+    private exchangeConnector: IExchangeConnector
+
+
+    public constructor(private aPIKey: string, private aPISecret: string) {
+
+        this.exchangeConnector = new BybitConnector(apiKey, apiSecret)
+
+    }
+
+
+    public async showUsageExamples(): Promise<void> {
+
+        const accountInfo = await this.exchangeConnector.getFuturesAccountData()
+
+        console.log(accountInfo)
+
+        const positions = await this.exchangeConnector.getPositions()
+
+        console.log(positions)
+
+    }
+
+}
+
+
+const apiKey = Deno.args[0]
+const apiSecret = Deno.args[1]
+
+const exampleClient = new ExampleClient(apiKey, apiSecret)
+
+await exampleClient.showUsageExamples()
