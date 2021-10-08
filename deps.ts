c51bd6bfef1e72302ac472a0f55b0cca4ba0215a
Michael <michael.spengler@sap.com>
2021-10-08 16:13:37 +0200

    ensure to be up to date

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/exchange_connectors@v1.4.0/mod-bybit.ts"
-export * from "https://deno.land/x/exchange_connectors@v1.4.0/mod-volatility-farmer.ts"
+export * from "https://deno.land/x/exchange_connectors@v1.4.2/mod-bybit.ts"
+export * from "https://deno.land/x/exchange_connectors@v1.4.2/mod-volatility-farmer.ts"
