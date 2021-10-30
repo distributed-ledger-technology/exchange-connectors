bcfce5a7f7a6bed1d98c08466017505b78c28f28
Michael <michael.spengler@sap.com>
2021-10-30 22:52:58 +0200

    logs

--- src/volatility-farming/investment-advisor/alternative-investment-advisors/investment-advisor-BTC-long-short-extreme.ts
+++ src/volatility-farming/investment-advisor/alternative-investment-advisors/investment-advisor-BTC-long-short-extreme.ts
@@ -104,11 +104,13 @@ export class InvestmentAdvisorBTCLongShortExtreme implements IInvestmentAdvisor 
 
         this.oPNLClosingLimit = Math.round(Math.random() * (81 - 45) + 45)
 
+        console.log(`overallPNL: ${overallPNL} vs. oPNLClosingLimit: ${this.oPNLClosingLimit} vs. liquidityLevel: ${this.liquidityLevel}`)
+
         if (this.liquidityLevel === 0) {
 
             this.closeAll(investmentOption, `a liquidity crisis - ll: ${this.liquidityLevel.toFixed(2)} - equity: ${investmentDecisionBase.accountInfo.result.USDT.equity.toFixed(2)}`)
 
-        } else if (overallPNL > this.oPNLClosingLimit && (this.liquidityLevel > 19 || this.liquidityLevel < 13)) {
+        } else if (overallPNL > this.oPNLClosingLimit && (this.liquidityLevel > 18 || this.liquidityLevel < 13)) {
 
             this.closeAll(investmentOption, `an overall PNL of ${overallPNL.toFixed(2)} - ll: ${this.liquidityLevel.toFixed(2)} - equity: ${investmentDecisionBase.accountInfo.result.USDT.equity}`)
 
