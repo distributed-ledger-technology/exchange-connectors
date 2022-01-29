152511ea750f346cd4c21783a021b5ef9410c31b
Michael <michael.spengler@sap.com>
2022-01-29 13:42:01 +0100

    add dydx related features

--- src/dydx/dydx-connector.spec.ts
+++ src/dydx/dydx-connector.spec.ts
@@ -11,29 +11,50 @@ Deno.test("should check if a user exists with wallet 0xD6C2DD6B5A5a3A58765eDcdcd
 
 })
 
-Deno.test("should retrieve account balance = wallet balance for 0xD6C2DD6B5A5a3A58765eDcdcd6c2768f5cdFd6D4", async () => {
+// Deno.test("should retrieve account balance = wallet balance for 0xD6C2DD6B5A5a3A58765eDcdcd6c2768f5cdFd6D4", async () => {
 
-    let actualBalance
-    let expectedBalance = 0
+//     let actualBalance
+//     let expectedBalance = 0
 
-    try {
-        actualBalance = dydxConnector.getBalance(testWallet)
-    } catch (error) {
-        fail(`the following unexpected error occurred: ${error}`)
-    }
+//     try {
+//         actualBalance = dydxConnector.getBalance(testWallet)
+//     } catch (error) {
+//         fail(`the following unexpected error occurred: ${error}`)
+//     }
 
-    assertEquals(actualBalance, expectedBalance)
+//     assertEquals(actualBalance, expectedBalance)
 
-})
+// })
 
 
-Deno.test("should return market data", async () => {
+// Deno.test("should return market data", async () => {
 
-    const result = await dydxConnector.getMarketData()
+//     const result = await dydxConnector.getMarketData()
 
-    if (result === undefined) {
-        fail(`I would have expected fancy market data`)
-    }
+//     if (result === undefined) {
+//         fail(`I would have expected fancy market data`)
+//     }
 
-})
+// })
+
+
+// Deno.test("should return account data", async () => {
+
+//     const result = await dydxConnector.getAccountData(testWallet)
+
+//     if (result === undefined) {
+//         fail(`I would have expected fancy account data`)
+//     }
+
+// })
+
+// Deno.test("should return open positions", async () => {
+
+//     const result = await dydxConnector.getPositions(testWallet)
+
+//     if (result === undefined) {
+//         fail(`I would have expected fancy account data`)
+//     }
+
+// })
 
--- src/dydx/dydx-connector.ts
+++ src/dydx/dydx-connector.ts
@@ -1,12 +1,45 @@
 
-// import Client from 'https://cdn.skypack.dev/@dydxprotocol/v3-client';
+// import Client from 'https://cdn.skypack.dev/@dydxprotocol/v3-client' // see issue
+
+// import dydxprotocolPerpetual from 'https://cdn.skypack.dev/@dydxprotocol/perpetual';
+import "https://deno.land/x/dotenv/load.ts";
+
 import { Request } from 'https://deno.land/x/request@1.3.2/mod.ts'
 
 export class DyDxConnector {
+
     private client: any
     private baseURL = "https://api.stage.dydx.exchange"
 
     public constructor() {
+
+        const walletPrivateKey = Deno.env.get("PRIVATEKEYFORDYDXINTERACTION")
+        // this.client = new Client(
+        //     'host',
+        //     {
+        //         apiTimeout: 3000,
+        //         starkPrivateKey: '01234abcd...',
+        //     },
+        // );
+
+    }
+
+    public async getPositions(walletAddress: string): Promise<any> {
+        const url = `${this.baseURL}/v3/positions?ethereumAddress=${walletAddress}`
+
+        const result = await Request.get(url)
+
+        return result
+
+    }
+
+    public async getAccountData(walletAddress: string): Promise<any> {
+        const url = `${this.baseURL}/v3/accounts?ethereumAddress=${walletAddress}`
+
+        const result = await Request.get(url)
+
+        return result
+
     }
 
     public async checkUserExists(walletAddress: string): Promise<boolean> {
